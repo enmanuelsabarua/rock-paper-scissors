f30b26f1adc864afc57a891502c7985bf9eb1b45
Enmanuel <enmanuel.sanchez.a@gmail.com>
2023-02-04 17:17:45 -0400

    Add basic UI

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const options = ["rock", "paper", "scissors"];
+let round = 0;
+let computerScore = 0;
+let playerScore = 0;
+let endGame = 0;
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
@@ -7,27 +11,27 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "rock") {
-        return "It's a tie!";
+        return ["It's a tie!", -1];
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You Lose! paper beats rock";
+        return ["You Lose! paper beats rock", 0];
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You Win! rock beats scissors";
+        return ["You Win! rock beats scissors", 1];
     }
 
     else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "It's a tie!";
+        return ["It's a tie!", -1];
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You Lose! scissors beats paper";
+        return ["You Lose! scissors beats paper", 0];
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You Win! paper beats rock";
+        return ["You Win! paper beats rock", 1];
     }
 
     else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "It's a tie!";
+        return ["It's a tie!", -1];
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You Lose! rock beats scissors";
+        return ["You Lose! rock beats scissors", 0];
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You Win! scissors beats paper";
+        return ["You Win! scissors beats paper", 1];
     } 
 
     else {
@@ -35,12 +39,53 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    const playerSelection = prompt("Rock Paper Scissors?");
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection.toLocaleLowerCase(), computerSelection));
-}
+const buttons = document.querySelectorAll('button');
 
-for (let i = 0; i < 5; i++) {
-    game();
-}
+buttons.forEach(button => {
+
+    button.addEventListener('click', e => {
+        const playScore = document.querySelector('.player');
+        const compScore = document.querySelector('.computer');
+
+        if (endGame) {
+            endGame = 0;
+            playerScore = 0;
+            computerScore = 0;
+            playScore.textContent = `Player: ${playerScore}`;
+            compScore.textContent = `Computer: ${computerScore}`;
+            return;
+        }
+        
+        let playerSelection = e.target.textContent;
+        const computerSelection = getComputerChoice();
+
+        const compSelection = document.querySelector('.computer-selection');
+        compSelection.textContent = ` Computer selection: ${computerSelection}`;
+        
+        const result = document.querySelector('.result');
+        result.textContent = playRound(playerSelection.toLocaleLowerCase(), computerSelection)[0];
+        
+        // If the player win, he wins a point, if the computer win it wins a point
+        if (playRound(playerSelection.toLocaleLowerCase(), computerSelection)[1] === 1) {
+            ++playerScore;
+            playScore.textContent = `Player: ${playerScore}`;
+        } else if (playRound(playerSelection.toLocaleLowerCase(), computerSelection)[1] === 0) {
+            ++computerScore;
+            compScore.textContent = `Computer: ${computerScore}`;
+        }
+        
+        // Check if the player or the computer won
+        if (playerScore === 5 || computerScore === 5) {
+            endGame = 1;
+
+            if (playerScore === 5) {
+                result.textContent = "You won the game!!";                
+                
+            } else if (computerScore === 5) {
+                result.textContent = "The computer won the game!!";
+                
+            }
+        }
+
+    });
+});
